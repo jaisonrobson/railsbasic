d6a500e36a0cc2bfe496b6f49dc74d1a1cfcaa1f
jasonrobson <xd_jrg@hotmail.com>
2019-05-05 19:35:48 -0300

    fix(comments): corrigido erro ao adicionar comentario

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    @comment = article.comments.create(comment_params)
+    @comment = @article.comments.create(comment_params)
 
     redirect_to article_path(@article)
   end
